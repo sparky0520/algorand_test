f69b24069544926df9c9549188b4866b791db3c4
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-09-06 15:00:39 +0530

    voting dapp init

--- projects/algorand_test/smart_contracts/hello_world/contract.algo.ts
+++ projects/algorand_test/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,29 @@
-import { Contract } from '@algorandfoundation/algorand-typescript'
+// import { Contract } from '@algorandfoundation/algorand-typescript'
 
-export class HelloWorld extends Contract {
-  hello(name: string): string {
-    return `Hello, ${name}`
+// export class HelloWorld extends Contract {
+//   hello(name: string): string {
+//     return `Hello, ${name}`
+//   }
+// }
+
+import { Contract } from "@algorandfoundation/tealscript";
+
+export class Voting extends Contract {
+  proposal = this.globalState.string({ key: "proposal", default: "" });
+  votes = this.globalState.uint64({ key: "votes", default: 0n });
+
+  createProposal(title: string): string {
+    this.proposal.value = title;
+    this.votes.value = 0n;
+    return this.proposal.value;
+  }
+
+  castVote(): bigint {
+    this.votes.value += 1n;
+    return this.votes.value;
+  }
+
+  getResult(): string {
+    return `Proposal: ${this.proposal.value}, Votes: ${this.votes.value}`;
   }
 }
